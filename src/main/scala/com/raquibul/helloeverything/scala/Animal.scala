40a6e6970020c85c9801f22162651fada9f0a264
raquibul <raquibul.hasan82@gmail.com>
2017-05-30 23:07:14 +0200

    keep only basic maven project

--- src/main/scala/com/raquibul/helloeverything/scala/Animal.scala
+++
@@ -1,7 +0,0 @@
-package com.raquibul.helloeverything.scala
-
-abstract class Animal {
-  def companion: AnimalCounter
-  companion.count();
-  companion.printHi;
-}
--- src/main/scala/com/raquibul/helloeverything/scala/Student.scala
+++
@@ -1,9 +0,0 @@
-package com.raquibul.helloeverything.scala
-
-class Student(id:Int, name:String) {
-  def showDetails() {
-    println(id + "|" + name)
-  }
-  
-  var flag = "P";
-}
